1e7e1dfdfc4cadf2b5c8876c34d0a95bcf83bc40
Derek Carr <zaphinath@gmail.com>
2013-10-07 10:22:42 -0600

    Adding init logs

--- src/QueueMain.java
+++ src/QueueMain.java
@@ -7,6 +7,12 @@ import java.io.IOException;
 
 import server.VMQueue;
 import java.sql.SQLException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import model.Job;
 import model.NamedPipeStream;
@@ -15,6 +21,30 @@ public class QueueMain {
 	private static File fPipe = new File("./pipe");
 	private static File fLock = new File("./pipe.lock");
 	private static boolean isLocked = false;
+	
+	private static Logger logger;
+	
+	static {
+		try {
+			Level logLevel = Level.FINE;
+			
+			logger = Logger.getLogger("vmqueue"); 
+			logger.setLevel(logLevel);
+			logger.setUseParentHandlers(false);
+			
+			Handler consoleHandler = new ConsoleHandler();
+			consoleHandler.setLevel(logLevel);
+			consoleHandler.setFormatter(new SimpleFormatter());
+			logger.addHandler(consoleHandler);
+
+			FileHandler fileHandler = new FileHandler("log.txt", false);
+			fileHandler.setLevel(logLevel);
+			fileHandler.setFormatter(new SimpleFormatter());
+			logger.addHandler(fileHandler);
+		} catch (IOException e) {
+			System.out.println("Could not initialize log: " + e.getMessage());
+		}
+	}
 	
   public static void main(String[] args) throws InterruptedException, IOException, SQLException {
   	int port = Integer.parseInt(args[0]);
