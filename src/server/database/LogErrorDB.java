be2858c1ae1fde186eca03ec03e55e096b7a1ecc
Derek Carr <zaphinath@gmail.com>
2013-10-21 08:15:57 -0600

    fixed query from copy and paste

--- src/server/database/LogErrorDB.java
+++ src/server/database/LogErrorDB.java
@@ -88,7 +88,7 @@ public class LogErrorDB {
 		PreparedStatement stmt = null;
 		try {
 			String sql = "INSERT INTO log_errors(log_queue_id, test_classname, test_name," +
-									 "test_time, failure_type, failure_message) values(?, ?, ?, ?, ? ,?)";
+									 "test_time, error_type, error_message) values(?, ?, ?, ?, ? ,?)";
 			stmt = db.getConnection().prepareStatement(sql);
 			
 			stmt.setInt(1, logError.getLogQueueId());
